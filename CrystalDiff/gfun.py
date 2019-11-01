de6f68e71b703cbfc55a5bee857b6d7607fb40ce
Haoyuan Li <hyli16@stanford.edu>
2019-10-31 20:42:41 -0700

    minor

--- CrystalDiff/gfun.py
+++ CrystalDiff/gfun.py
@@ -445,10 +445,12 @@ def get_bragg_reflection(reflectivity_sigma, reflectivity_pi, kout_grid, efield_
 
         # Get the polarization factor with the asymmetric factor b.
         sin_theta = (dot_kh - h_square - m_trans * dot_hn) / klen / h_len
-        b_polar = complex(b * ((1 - 2 * sin_theta ** 2) ** 2))
+
+        p_value = 1 - 2 * sin_theta ** 2
+        b_polar = complex(b * p_value)
 
         # Get sqrt(alpha**2 + beta**2) value
-        sqrt_a2_b2 = cmath.sqrt(alpha_tidle ** 2 + b_polar * chih_pi * chihbar_pi)
+        sqrt_a2_b2 = cmath.sqrt(alpha_tidle ** 2 + b_polar * p_value * chih_pi * chihbar_pi)
 
         # Because this is a thick crystal, only one mode will be activated.
         if sqrt_a2_b2.imag < 0:
