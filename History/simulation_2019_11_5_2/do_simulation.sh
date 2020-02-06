1c1286e9adc35ce03534ec8cd5bab88b36c7feaf
Haoyuan Li <hyli16@stanford.edu>
2020-02-06 14:58:52 -0800

    clean up and release

--- History/simulation_2019_11_5_2/do_simulation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python ~/my_repos/CrystalDiff/Simulation/inclined_1fs.py ; \
-python ~/my_repos/CrystalDiff/Simulation/inclined_100fs.py ; \
-python ~/my_repos/CrystalDiff/Simulation/inclined_100as.py ; \
-python ~/my_repos/CrystalDiff/Simulation/delay_line_1fs.py ; \
-python ~/my_repos/CrystalDiff/Simulation/delay_line_100fs.py ; \
-python ~/my_repos/CrystalDiff/Simulation/delay_line_100as.py ; \
-python ~/my_repos/CrystalDiff/Simulation/shearing_fix.py
